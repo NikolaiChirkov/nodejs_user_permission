c134b6ce87d32a557dadec2270b1dfaa77880ea7
Nikolai Chirkov <qwertymonstro@gmail.com>
2020-10-20 12:55:44 +0300

    individual project permission

--- routes/projects.js
+++ routes/projects.js
@@ -1,24 +1,35 @@
-const express = require('express')
-const router = express.Router()
-const { projects } = require('../data')
+const express = require('express');
+const { authUser } = require('../basicAuth');
+const router = express.Router();
+const { projects } = require('../data');
+const { canViewProject } = require('../permissions/project');
 
 router.get('/', (req, res) => {
-  res.json(projects)
+  res.json(projects);
 })
 
-router.get('/:projectId', setProject, (req, res) => {
-  res.json(req.project)
+router.get('/:projectId', setProject, authUser, authGetProject, (req, res) => {
+  res.json(req.project);
 })
 
 function setProject(req, res, next) {
-  const projectId = parseInt(req.params.projectId)
-  req.project = projects.find(project => project.id === projectId)
+  const projectId = parseInt(req.params.projectId);
+  req.project = projects.find(project => project.id === projectId);
   
   if (req.project == null) {
-    res.status(404)
-    return res.send('Project not found')
+    res.status(404);
+    return res.send('Project not found');
   }
-  next()
+  next();
 }
 
-module.exports = router
+function authGetProject(req, res, next) {
+  if (!canViewProject(req.user, req.project)) {
+    res.status(401);
+    return res.send('Not allowed');
+  }
+
+  next();
+}
+
+module.exports = router;
