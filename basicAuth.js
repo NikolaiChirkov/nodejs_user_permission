aa65cfa7e8d22fb71f0516b5ccb93a8f10b5a062
Nikolai Chirkov <qwertymonstro@gmail.com>
2020-10-20 12:42:50 +0300

    authRole

--- basicAuth.js
+++ basicAuth.js
@@ -7,4 +7,15 @@ function authUser(req, res, next) {
     next();
 }
 
-module.exports = { authUser };
+function authRole(role) {
+    return (req, res, next) => {
+        if (req.user.role !== role) {
+            res.status(401);
+            return res.send("Not allowed");
+        }
+
+        next();
+    }
+}
+
+module.exports = { authUser, authRole };
